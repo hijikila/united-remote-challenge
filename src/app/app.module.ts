bd7f37dd1537e3eb7c8f232985eb5bdcf0b3cb1d
hijikila <youssefgoumehri@gmail.com>
2019-09-11 16:11:54 +0100

    Planning general structure of the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { RepositoryItemComponent } from './repository/repository-item/repository-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent,
+    RepositoryItemComponent
   ],
   imports: [
     BrowserModule
