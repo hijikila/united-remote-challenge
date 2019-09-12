3b3def3db879d8d6623de695b309b672dd8eab43
hijikila <youssefgoumehri@gmail.com>
2019-09-12 22:21:57 +0100

    Added shared module and repository module, routing and lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RepositoryComponent } from './repository/repository.component';
-import { RepositoryItemComponent } from './repository/repository-item/repository-item.component';
-import { ShortenNumberPipe } from './shared/pipes/shorten-number.pipe';
-import { ShortenTextPipe } from './shared/pipes/shorten-text.pipe';
 import { HttpClientModule } from '@angular/common/http';
-import { InfiniteScrollDirective } from './shared/directives/infinite-scroll.directive';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import {ErrorAlertComponent} from './shared/error-alert/error-alert.component';
+import {RepositoryModule} from './repository/repository.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RepositoryComponent,
-    RepositoryItemComponent,
-    ShortenNumberPipe,
-    ShortenTextPipe,
-    InfiniteScrollDirective,
-    LoadingSpinnerComponent,
-    ErrorAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RepositoryModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
